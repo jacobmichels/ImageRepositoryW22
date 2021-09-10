6eabe8833b5b2a472b912f913587850e3db60563
Jacob Michels <jacob.michels2025@gmail.com>
2021-09-10 18:02:02 -0400

    Add project files.

---
+++ ImageRepositoryW22/Migrations/20210910213608_InitialMigration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ImageRepositoryW22.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UserName = table.Column<string>(type: "TEXT", nullable: false),
+                    PasswordHash = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ ImageRepositoryW22/Repository/Models/ApplicationUser.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageRepositoryW22.Repository.Models
+{
+    public class ApplicationUser
+    {
+        [Key]
+        public Guid Id {  get; set; }
+        [Required]
+        public string UserName {  get; set; }
+        [Required]
+        public string PasswordHash {  get; set; }
+
+        public ApplicationUser(string username, string passwordhash)
+        {
+            UserName = username;
+            PasswordHash = passwordhash;
+        }
+
+        public ApplicationUser()
+        {
+        }
+    }
+}
