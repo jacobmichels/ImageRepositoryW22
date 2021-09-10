a36bd3ebdfec8a54e1239bc7d74350a94e8c9a75
Jacob Michels <jacob.michels2025@gmail.com>
2021-09-10 18:27:27 -0400

    create image class

---
+++ ImageRepositoryW22/Repositories/Models/Image.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageRepositoryW22.Repositories.Models
+{
+    public class Image
+    {
+        [Key]
+        public Guid Id {  get; set; }
+        [Required]
+        public string Name {  get; set; }
+        [Required]
+        public string Description {  get; set; }
+        [Required]
+        public string Path {  get; set; }
+        [Required]
+        public ApplicationUser Owner {  get; set; }
+        public bool Private { get; set; }
+    }
+}
