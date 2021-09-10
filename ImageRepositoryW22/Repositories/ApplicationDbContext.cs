54c57d765e02c6a621be27a4ae827ab5f3208953
Jacob Michels <jacob.michels2025@gmail.com>
2021-09-10 18:30:26 -0400

    re-add due to folder name change

---
+++ ImageRepositoryW22/Repositories/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ImageRepositoryW22.Repositories.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageRepositoryW22.Repositories
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public DbSet<ApplicationUser> Users {  get; set; }
+        public DbSet<Image> Images { get; set; }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+    }
+}
