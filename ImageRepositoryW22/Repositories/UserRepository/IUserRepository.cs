e63429d2df1b86e948983e082f707993c0ec1fcf
Jacob Michels <jacob.michels2025@gmail.com>
2021-09-10 18:29:26 -0400

    create user repository and interface

---
+++ ImageRepositoryW22/Repositories/UserRepository/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using ImageRepositoryW22.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageRepositoryW22.Repositories.UserRepository
+{
+    public interface IUserRepository
+    {
+        public Task<ApplicationUser> GetUser(string username);
+        public Task<bool> UserExists(string username);
+        public Task<ApplicationUser> CreateUser(string username, string password);
+        public Task<bool> DeleteUser(string username);
+    }
+}
---
+++ ImageRepositoryW22/Repositories/UserRepository/UserRepository.cs
@@ -0,0 +1,31 @@
+﻿using ImageRepositoryW22.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageRepositoryW22.Repositories.UserRepository
+{
+    public class UserRepository : IUserRepository
+    {
+        public Task<ApplicationUser> CreateUser(string username, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ApplicationUser> GetUser(string username)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UserExists(string username)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
