1a63378b441b81f590420d78d75cd338f62853a4
Jacob Michels <jacob.michels2025@gmail.com>
2021-09-10 18:29:16 -0400

    create image repository and interface

---
+++ ImageRepositoryW22/Repositories/ImageRepository/IImageRepository.cs
@@ -0,0 +1,24 @@
+﻿using ImageRepositoryW22.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageRepositoryW22.ImageRepository.Repositories
+{
+    public interface IImageRepository
+    {
+        public List<Image> GetAll();
+        public List<Image> GetAll(ApplicationUser user);
+        public Image Get(ApplicationUser user, Guid id);
+
+        public bool Create(ApplicationUser user, Image image);
+        public bool Create(ApplicationUser user, List<Image> images);
+
+        public Image Update(ApplicationUser user, Image image);
+
+        public bool Delete(ApplicationUser user, Guid id);
+        public int Delete(ApplicationUser user, List<Guid> ids);
+
+    }
+}
---
+++ ImageRepositoryW22/Repositories/ImageRepository/ImageRepository.cs
@@ -0,0 +1,54 @@
+﻿using ImageRepositoryW22.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageRepositoryW22.ImageRepository.Repositories
+{
+    public class ImageRepository : IImageRepository
+    {
+        public bool Create(ApplicationUser user, Image image)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Create(ApplicationUser user, List<Image> images)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(ApplicationUser user, Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Delete(ApplicationUser user, List<Guid> ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Return image data only here.
+        public Image Get(ApplicationUser user, Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        //Don't return image data, just info like name
+        public List<Image> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        //Don't return image data, just info like name
+        public List<Image> GetAll(ApplicationUser user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Image Update(ApplicationUser user, Image image)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
