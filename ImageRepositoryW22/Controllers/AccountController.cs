068c77e146afc6af178a2ace7a2bc56f5be9e556
Jacob Michels <jacob.michels2025@gmail.com>
2021-09-10 18:29:59 -0400

    create controller for account endpoints

---
+++ ImageRepositoryW22/Controllers/AccountController.cs
@@ -0,0 +1,54 @@
+﻿using ImageRepositoryW22.Repositories;
+using ImageRepositoryW22.Repositories.UserRepository;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageRepositoryW22.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class AccountController: ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public AccountController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(string username, string password)
+        {
+            return null;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(string username, string password)
+        {
+            return null;
+
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Logout(string username, string password)
+        {
+            return null;
+
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<IActionResult> Delete(string username, string password)
+        {
+            return null;
+
+        }
+    }
+}
