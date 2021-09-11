6c7c25bb590ae6960189f8943afec341fa5517e5
Jacob Michels <jacob.michels2025@gmail.com>
2021-09-11 01:03:27 -0400

    roughly complete image controller

--- ImageRepositoryW22/Controllers/ImageController.cs
+++ ImageRepositoryW22/Controllers/ImageController.cs
@@ -1,6 +1,7 @@
 ﻿using ImageRepositoryW22.ImageRepository.Repositories;
 using ImageRepositoryW22.Repositories.Models;
 using ImageRepositoryW22.Repositories.UserRepository;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -16,50 +17,84 @@ namespace ImageRepositoryW22.Controllers
     {
         private readonly IImageRepository _imageRepository;
         private readonly IUserRepository _userRepository;
+        private readonly string _username;
         public ImageController(IImageRepository imageRepository, IUserRepository userRepository)
         {
             _imageRepository = imageRepository;
             _userRepository = userRepository;
+            _username = HttpContext.User.Identity.Name;
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> Get(Guid id) {
-            return null;
+            var user = await _userRepository.GetUser(_username);
+            var image = await _imageRepository.Get(user, id);
+            return Ok(image);
         }
 
+        [Authorize]
         [HttpGet]
         public async Task<IActionResult> GetMine()
         {
-            return null;
+            var user = await _userRepository.GetUser(_username);
+            var images = await _imageRepository.GetAll(user);
+            return Ok(images);
         }
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetAllPublic()
         {
-            return null;
+            var images = await _imageRepository.GetAll();
+            return Ok(images);
         }
 
+        [Authorize]
         [HttpPost]
         public async Task<IActionResult> CreateOne([FromForm] RequestImage imageInfo)
         {
-            return null;
+            var user = await _userRepository.GetUser(_username);
+            var created = await _imageRepository.Create(user, imageInfo);
+            if (created)
+            {
+                return Ok();
+            }
+            return BadRequest("Image was not created. Make sure it does not have the same name as another one of your images.");
         }
 
+        [Authorize]
         [HttpPatch]
-        public async Task<IActionResult> Update(RequestImage image, Guid id)
+        public async Task<IActionResult> Update(ImageUpdate image, Guid id)
         {
-            return null;
+            var user = await _userRepository.GetUser(_username);
+            var updated = await _imageRepository.Update(user, image, id);
+            if(updated is not null)
+            {
+                return Ok(updated);
+            }
+            return BadRequest("Problem updating image.");
         }
 
+        [Authorize]
         [HttpDelete]
         public async Task<IActionResult> DeleteOne(Guid id)
         {
-            return null;
+            var user = await _userRepository.GetUser(_username);
+            var deleted = await _imageRepository.Delete(user, id);
+            if (deleted)
+            {
+                return Ok();
+            }
+            return BadRequest("Image not deleted");
         }
 
+        [Authorize]
         [HttpDelete]
-        public async Task<IActionResult> DeleteMany(List<Guid> id)
+        public async Task<IActionResult> DeleteMany(List<Guid> ids)
         {
-            return null;
+            var user = await _userRepository.GetUser(_username);
+            var deleted = await _imageRepository.Delete(user, ids);
+            return Ok($"Deleted {deleted} images.");
         }
     }
 }
