d9ef7ce30bbad5440bd5d20afa503c92f102b05c
Jacob Michels <jacob.michels2025@gmail.com>
2021-09-10 22:06:57 -0400

    rename image models file, and add model for endpoint input images

--- ImageRepositoryW22/Repositories/Models/ImageModels.cs
+++ ImageRepositoryW22/Repositories/Models/ImageModels.cs
@@ -1,23 +1,35 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Http;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ImageRepositoryW22.Repositories.Models
 {
-    public class Image
+    public class DatabaseImage
     {
         [Key]
         public Guid Id {  get; set; }
         [Required]
         public string Name {  get; set; }
-        [Required]
         public string Description {  get; set; }
         [Required]
         public string Path {  get; set; }
         [Required]
         public ApplicationUser Owner {  get; set; }
         public bool Private { get; set; }
+    }
+
+    public class RequestImage
+    {
+        [Required]
+        public string Name {  get; set; }
+        public string Description { get; set; }
+        [Required]
+        public bool Private { get; set; }
+        [Required]
+        public IFormFile File { get; set; }
     }
 }
