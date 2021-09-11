4d6c2a2a02b4ae19d1ebae3328974f8990bea013
Jacob Michels <jacob.michels2025@gmail.com>
2021-09-11 01:02:04 -0400

    roughly complete account controller

--- ImageRepositoryW22/Controllers/AccountController.cs
+++ ImageRepositoryW22/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
 ﻿using ImageRepositoryW22.Repositories;
+using ImageRepositoryW22.Repositories.Models;
 using ImageRepositoryW22.Repositories.UserRepository;
+using ImageRepositoryW22.Utilities.PasswordUtilities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -14,41 +16,79 @@ namespace ImageRepositoryW22.Controllers
     public class AccountController: ControllerBase
     {
         private readonly IUserRepository _userRepository;
+        private readonly IPasswordUtilities _passwordUtils;
 
-        public AccountController(IUserRepository userRepository)
+        public AccountController(IUserRepository userRepository, IPasswordUtilities passwordUtils)
         {
             _userRepository = userRepository;
+            _passwordUtils = passwordUtils;
         }
 
         [HttpPost]
         [AllowAnonymous]
         public async Task<IActionResult> Login(string username, string password)
         {
-            return null;
+            var user = await _userRepository.GetUser(username);
+            if(user is null)
+            {
+                return Forbid();
+            }
+            if(!await _passwordUtils.VerifyPasswordAysnc(password, user.PasswordHash))
+            {
+                return Forbid();
+            }
+            //TODO: Generate JWT and return it
+            return Ok();
         }
 
         [HttpPost]
         [AllowAnonymous]
+        //TODO: Make sure users cannot register with a blank or whitespace only username or password
         public async Task<IActionResult> Register(string username, string password)
         {
-            return null;
+            if(await _userRepository.UserExists(username))
+            {
+                return Forbid("A user with that name already exists");
+            }
+            var user = await CreateApplicationUser(username, password);
+            var userInserted = await _userRepository.InsertUser(user);
 
+            if (!userInserted)
+            {
+                return Problem();
+            }
+
+            return Ok();
         }
 
         [HttpPost]
         [Authorize]
-        public async Task<IActionResult> Logout(string username, string password)
+        //TODO: Decide if this method should exist, and what it should do. Should it invalidate the JWT?
+        public async Task<IActionResult> Logout()
         {
             return null;
-
         }
 
         [HttpDelete]
         [Authorize]
-        public async Task<IActionResult> Delete(string username, string password)
+        //TODO:Should this method invalidate the JWT?
+        public async Task<IActionResult> Delete()
         {
-            return null;
+            var username = HttpContext.User.Identity.Name;
+            var deleted = await _userRepository.DeleteUser(username);
+            if (!deleted)
+            {
+                return BadRequest("User not deleted");
+            }
 
+            return Ok();
         }
+
+        private async Task<ApplicationUser> CreateApplicationUser(string username, string password)
+        {
+            var hashedPassword = await _passwordUtils.HashPasswordAsync(password);
+            var user = new ApplicationUser(username, hashedPassword);
+            return user;
+        }    
     }
 }
