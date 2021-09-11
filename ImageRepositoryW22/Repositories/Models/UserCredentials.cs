733721df5bb71de356bd6ac62eb08fbb8e5e5d90
Jacob Michels <jacob.michels2025@gmail.com>
2021-09-11 16:30:43 -0400

    add user credentials class for credentials input

---
+++ ImageRepositoryW22/Repositories/Models/UserCredentials.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageRepositoryW22.Repositories.Models
+{
+    public class UserCredentials
+    {
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
