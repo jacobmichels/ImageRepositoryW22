0d4bba0015d55df48b8e620e47ea25362d35e03c
Jacob Michels <jacob.michels2025@gmail.com>
2021-09-11 01:03:05 -0400

    add argon2 password hashing utility class

---
+++ ImageRepositoryW22/Utilities/PasswordUtilities/IPasswordUtilities.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImageRepositoryW22.Utilities.PasswordUtilities
+{
+    public interface IPasswordUtilities
+    {
+        public Task<string> HashPasswordAsync(string password);
+
+        public Task<bool> VerifyPasswordAysnc(string password, string storedHash);
+    }
+}
---
+++ ImageRepositoryW22/Utilities/PasswordUtilities/PasswordUtilities.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+using Konscious.Security.Cryptography;
+
+namespace ImageRepositoryW22.Utilities.PasswordUtilities
+{
+    public class PasswordUtilities : IPasswordUtilities
+    {
+        public async Task<string> HashPasswordAsync(string password)
+        {
+            var argon2 = new Argon2d(Encoding.UTF8.GetBytes(password))
+            {
+                DegreeOfParallelism = 32,
+                MemorySize = 16392,
+                Iterations = 48,
+                Salt = Encoding.UTF8.GetBytes("caa4e318-3b48-4509-bb41-00aaa13e05d4")
+            };
+
+            return Encoding.UTF8.GetString(await argon2.GetBytesAsync(30));
+        }
+
+        public async Task<bool> VerifyPasswordAysnc(string password, string storedHash)
+        {
+            var argon2 = new Argon2d(Encoding.UTF8.GetBytes(password))
+            {
+                DegreeOfParallelism = 32,
+                MemorySize = 16392,
+                Iterations = 48,
+                Salt = Encoding.UTF8.GetBytes("caa4e318-3b48-4509-bb41-00aaa13e05d4")
+            };
+
+            var passwordHash = Encoding.UTF8.GetString(await argon2.GetBytesAsync(30));
+
+            return passwordHash == storedHash;
+        }
+    }
+}
